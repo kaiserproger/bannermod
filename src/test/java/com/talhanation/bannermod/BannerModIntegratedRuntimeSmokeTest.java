c89940131b2633b7052f70ec241fc90c2b7e2a2c
pozdn.r.a <kaisergrobe@gmail.com>
2026-05-08 15:42:49 +0700

    test civilian packet catalog size

--- src/test/java/com/talhanation/bannermod/BannerModIntegratedRuntimeSmokeTest.java
+++ src/test/java/com/talhanation/bannermod/BannerModIntegratedRuntimeSmokeTest.java
@@ -16,7 +16,7 @@ class BannerModIntegratedRuntimeSmokeTest {
         assertEquals(BannerModMain.MOD_ID, WorkersRuntime.modId());
         assertEquals(BannerModNetworkBootstrap.workerPacketOffset(), WorkersRuntime.networkIdOffset());
         assertEquals(BannerModNetworkBootstrap.MILITARY_MESSAGES.length, BannerModNetworkBootstrap.workerPacketOffset());
-        assertEquals(32, BannerModNetworkBootstrap.CIVILIAN_MESSAGES.length);
+        assertEquals(33, BannerModNetworkBootstrap.CIVILIAN_MESSAGES.length);
         assertTrue(BannerModNetworkBootstrap.CIVILIAN_MESSAGES.length > 0);
     }
 }
